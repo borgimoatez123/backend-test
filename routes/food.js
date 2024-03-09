1240d5b0763a0a4113469bd83112949426b35946
borgimoatez123 <borgimoatez35@gmail.com>
2024-03-09 11:41:47 -0800

    frist commit

---
+++ routes/food.js
@@ -0,0 +1,25 @@
+const router = require('express').Router();
+const foodController = require('../controllers/foodController')
+const { verifyVendor } = require('../middleware/verifyToken')
+
+router.post('/', verifyVendor, foodController.addFood)
+
+router.post('/tag/:id', verifyVendor, foodController.addFoodTag)
+
+router.post('/type/:id', verifyVendor, foodController.addFoodType)
+
+router.get('/:id', foodController.getFoodById)
+
+router.get('/:category/:code', foodController.getRandomByCategoryAndCode)
+
+router.delete('/:id', verifyVendor, foodController.deleteFoodById)
+
+router.patch('/:id', verifyVendor, foodController.foodAvailability)
+
+router.get('/restaurant/:restaurantId', foodController.getFoodByRestaurant)
+
+router.get('/restaurant/:restaurantId', foodController.getFoodByRestaurant)
+
+
+
+module.exports = router;
